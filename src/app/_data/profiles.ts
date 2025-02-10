91ba206d863e2c400589dc686767ea36942fd130
dinogomez <dinogomez117@gmail.com>
2025-02-11 00:32:09 +0800

    feat: added landing profile page added working navbar with profile selection added avatars and pioflix logo added dummy accounts

---
+++ src/app/_data/profiles.ts
@@ -0,0 +1,22 @@
+export const profiles = [
+  {
+    id: 1,
+    name: "Stephen",
+    image: "/avatar/sonic.png",
+  },
+  {
+    id: 2,
+    name: "Samantha",
+    image: "/avatar/amy.png",
+  },
+  {
+    id: 3,
+    name: "Jay",
+    image: "/avatar/rouge.png",
+  },
+  {
+    id: 4,
+    name: "Paul",
+    image: "/avatar/rebel_rouge.png",
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,37 @@
 // app/page.tsx
-import { MovieCard } from "@/components/movie-card";
-import { Movie } from "@/types/movie";
+import Image from 'next/image'
+import Link from 'next/link'
+import { profiles } from './_data/profiles'
 
-export default async function Home() {
-  const movies = await fetch("http://localhost:3000/api/movies", {
-    next: { revalidate: 3600 },
-  }).then((res) => res.json());
 
+
+export default function ProfileSelection() {
   return (
-    <main className="container mx-auto py-8">
-      <h1 className="mb-8 text-3xl font-bold">Upcoming Movies</h1>
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {movies.results.map((movie: Movie) => (
-          <MovieCard key={movie.id} movie={movie} />
+    <main className="min-h-screen text-white flex flex-col items-center justify-center">
+      <h1 className="text-4xl mb-12">Who's watching?</h1>
+
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+        {profiles.map((profile) => (
+          <Link
+            href={`/browse?profile=${profile.id}`}
+            key={profile.id}
+            className="group flex flex-col items-center gap-3"
+          >
+            <div className="relative w-32 h-32 overflow-hidden rounded-lg group-hover:ring-4 ring-white transition-all">
+              <Image
+                src={profile.image}
+                alt={profile.name}
+                fill
+                className="object-cover group-hover:scale-105 transition-transform"
+              />
+            </div>
+            <span className="text-gray-400 group-hover:text-white transition-colors">
+              {profile.name}
+            </span>
+          </Link>
         ))}
       </div>
+
     </main>
-  );
+  )
 }
