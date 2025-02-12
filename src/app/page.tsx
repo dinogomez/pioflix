a85ac257001a1485a751622b8135b529298f3695
dinogomez <dinogomez117@gmail.com>
2025-02-13 00:44:55 +0800

    fix: cosmetic loading fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,12 @@
-"use client"
-
+// app/page.tsx
 import { profiles } from "@/lib/profiles";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Link } from 'next-view-transitions';
 import ImageFallback from "./_components/ImageFallback";
-import Loading from "./loading";
 
-export default function ProfileSelection() {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
 
-  const handleProfileClick = (profileId: string) => {
-    setIsLoading(true);
-    router.push(`/?profile=${profileId}`);
-    setTimeout(() => {
-      router.push(`/browse/?profile=${profileId}`);
-    }, 1600);
-  };
 
-  if (isLoading) {
-    return <Loading />;
-  }
+export default async function ProfileSelection() {
+
 
   return (
     <main className="min-h-screen text-white flex flex-col items-center justify-center">
@@ -28,26 +14,26 @@ export default function ProfileSelection() {
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
         {profiles.map((profile) => (
-          <button
-            key={profile.id.toString()}
+          <Link
+            href={`/browse/?profile=${profile.id}`}
+            key={profile.id}
             className="group flex flex-col items-center gap-3"
-            onClick={() => handleProfileClick(profile.id.toString())}
           >
-            <div className="relative w-40 h-40 overflow-hidden rounded-lg group-hover:ring-2 ring-white transition-all">
+            <div className="relative w-40 h-40 overflow-hidden rounded-lg group-hover:ring-4 ring-white transition-all">
               <ImageFallback
                 src={profile.image}
                 alt={profile.name ? profile.name : "profile avatar"}
                 fill
-                priority
                 className="object-cover group-hover:scale-105 transition-transform"
               />
             </div>
             <span className="text-gray-400 group-hover:text-white transition-colors">
               {profile.name}
             </span>
-          </button>
+          </Link>
         ))}
       </div>
+
     </main>
-  );
+  )
 }
