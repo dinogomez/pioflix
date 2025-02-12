5d6b4c232d0a0a1e334c152f7788514ca1c9d858
dinogomez <dinogomez117@gmail.com>
2025-02-13 00:26:34 +0800

    fix: cosmetic loading fix

--- src/app/browse/_components/_params/ProfileParams.tsx
+++
@@ -1,9 +0,0 @@
-import { parseAsString } from "nuqs/server"
-
-import { createLoader } from "nuqs/server"
-
-export const profileSearchParams = {
-    profile: parseAsString
-}
-
-export const loadProfileParams = createLoader(profileSearchParams)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
-// app/page.tsx
+"use client"
+
 import { profiles } from "@/lib/profiles";
-import { Link } from 'next-view-transitions';
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import ImageFallback from "./_components/ImageFallback";
+import Loading from "./loading";
 
+export default function ProfileSelection() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
+  const handleProfileClick = (profileId: string) => {
+    setIsLoading(true);
+    router.push(`/?profile=${profileId}`);
+    setTimeout(() => {
+      router.push(`/browse/?profile=${profileId}`);
+    }, 1600);
+  };
 
-export default async function ProfileSelection() {
-
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <main className="min-h-screen text-white flex flex-col items-center justify-center">
@@ -14,26 +28,26 @@ export default async function ProfileSelection() {
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
         {profiles.map((profile) => (
-          <Link
-            href={`/browse/loading-redirect?profile=${profile.id}`}
-            key={profile.id}
+          <button
+            key={profile.id.toString()}
             className="group flex flex-col items-center gap-3"
+            onClick={() => handleProfileClick(profile.id.toString())}
           >
-            <div className="relative w-40 h-40 overflow-hidden rounded-lg group-hover:ring-4 ring-white transition-all">
+            <div className="relative w-40 h-40 overflow-hidden rounded-lg group-hover:ring-2 ring-white transition-all">
               <ImageFallback
                 src={profile.image}
                 alt={profile.name ? profile.name : "profile avatar"}
                 fill
+                priority
                 className="object-cover group-hover:scale-105 transition-transform"
               />
             </div>
             <span className="text-gray-400 group-hover:text-white transition-colors">
               {profile.name}
             </span>
-          </Link>
+          </button>
         ))}
       </div>
-
     </main>
-  )
+  );
 }
