0fb23c8f8f282af2f6c95d861f01685db3b08389
dinogomez <dinogomez117@gmail.com>
2025-02-13 06:13:36 +0800

    feat: added explore all links with pagination

--- src/app/api/random/[type]/route.ts
+++ src/app/api/random/[type]/route.ts
@@ -23,7 +23,7 @@ export async function GET(
 
     const randomContent = content[Math.floor(Math.random() * content.length)];
     return NextResponse.json(randomContent);
-  } catch (error) {
+  } catch {
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
