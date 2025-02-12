e0488778b173fc4be0aefa7de8337a11a87823e7
dinogomez <dinogomez117@gmail.com>
2025-02-13 03:18:48 +0800

    fix: navbar content shift issue

--- src/components/icons/bell-icon.tsx
+++ src/components/icons/bell-icon.tsx
@@ -61,8 +61,8 @@ const BellIcon = forwardRef<BellIconHandle, HTMLAttributes<HTMLDivElement>>(
             >
                 <motion.svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="28"
-                    height="28"
+                    width="24"
+                    height="24"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
