797991521e81412dd9f34e18b9b7ddde917b702b
dinogomez <dinogomez117@gmail.com>
2025-02-13 02:09:22 +0800

    feat: added new icons and new notification

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,23 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-        port: '',
-        pathname: '/**',
-        search: '',
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function calculateDaysLeft(targetDate: string): string {
+  const today = new Date();
+  const target = new Date(targetDate);
+
+  today.setHours(0, 0, 0, 0);
+  target.setHours(0, 0, 0, 0);
+
+  const diffTime = target.getTime() - today.getTime();
+  const diffDays = Math.abs(Math.ceil(diffTime / (1000 * 60 * 60 * 24)));
+
+  if (diffDays === 0) {
+    return "present";
+  } else if (diffDays === 1) {
+    return "1 day";
+  } else {
+    return `${diffDays} days ago`;
+  }
+}
