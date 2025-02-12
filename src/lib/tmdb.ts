e93a9483bf82ee6cb8a183783283484578ecd965
dinogomez <dinogomez117@gmail.com>
2025-02-12 15:39:49 +0800

    fix: deployment fix dynamic base url

--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -1,6 +1,8 @@
+import { env } from "./env";
+
 export async function getPopularMovies() {
   const res = await fetch(
-    `${process.env.TMDB_API_BASE_URL}/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+    `${env.tmdb.baseUrl}/movie/popular?api_key=${env.tmdb.apiKey}&language=en-US&page=1`
   );
   const data = await res.json();
   return data.results;
